2750f49b7d1f0f095a0fbb649f87147860c08576
Kimiko Kano <kskano@gmail.com>
2016-05-31 10:45:06 -0600

    Changed test to remove extra args from tile creation

--- test/tile-inventory-test.js
+++ test/tile-inventory-test.js
@@ -11,9 +11,9 @@ describe('TileInventory', () => {
   describe('isPositionOpen', () => {
     it('returns true if tile not in given position', () => {
       let tileInventory = new TileInventory();
-      let tile1 = new Tile("image.jpg", "A", "field", "road", "field", "city", false, 100, 100);
-      let tile2 = new Tile("image.jpg", "A", "field", "road", "field", "city", false, 200, 200);
-      let tile3 = new Tile("image.jpg", "A", "field", "road", "field", "city", false, 300, 300);
+      let tile1 = new Tile("A", 100, 100);
+      let tile2 = new Tile("A", 200, 200);
+      let tile3 = new Tile("A", 300, 300);
       tileInventory.tilesPlayed = [tile1, tile2, tile3];
       assert(tileInventory.isPositionOpen(400, 400));
       assert.isFalse(tileInventory.isPositionOpen(100, 100));
@@ -23,9 +23,9 @@ describe('TileInventory', () => {
   describe('addAdjacentCoordinates', () => {
     it('adds x and y coordinates of position around tile to adjacentPositions array', () => {
       let tileInventory = new TileInventory();
-      let tile1 = new Tile("image.jpg", "A", "field", "road", "field", "city", false, 100, 100);
-      let tile2 = new Tile("image.jpg", "A", "field", "road", "field", "city", false, 200, 200);
-      let tile3 = new Tile("image.jpg", "A", "field", "road", "field", "city", false, 300, 300);
+      let tile1 = new Tile("A", 100, 100);
+      let tile2 = new Tile("A", 200, 200);
+      let tile3 = new Tile("A", 300, 300);
 
       tileInventory.addAdjacentCoordinates(500, 500);
       let expected = [ [ 500, 400 ], [ 600, 500 ], [ 500, 600 ], [ 400, 500 ] ]
--- test/tile-test.js
+++ test/tile-test.js
@@ -3,25 +3,19 @@ const Tile = require('../lib/tile');
 
 describe('Tile', () => {
   it('returns an object', () => {
-    let tile = new Tile("image.jpg", "A", "field", "road", "field", "city", false, 100, 100);
+    let tile = new Tile("A", 100, 100);
     assert.typeOf(tile, 'object');
   });
 
   it('has attributes', () => {
-    let tile = new Tile("image.jpg", "A", "field", "road", "field", "city", false, 100, 100);
-    assert.equal(tile.image, "image.jpg");
+    let tile = new Tile("A", 100, 100);
     assert.equal(tile.type, "A");
-    assert.equal(tile.top, "field");
-    assert.equal(tile.right, "road");
-    assert.equal(tile.bottom, "field");
-    assert.equal(tile.left, "city");
-    assert.equal(tile.pennant, false);
     assert.equal(tile.x, 100);
     assert.equal(tile.y, 100);
   });
 
   it('has x and y that both default to zero', () => {
-    let tile = new Tile("image.jpg", "A", "field", "road", "field", "city", false);
+    let tile = new Tile("A");
     assert.equal(tile.x, 0);
     assert.equal(tile.y, 0);
   });
